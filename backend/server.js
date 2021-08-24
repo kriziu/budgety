fadec20c241a45c41c386b3c9efd1da93ecc8936
kriziu <brunodzi07@gmail.com>
2021-08-25 01:00:09 +0200

    made backend server

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const serverless = require('serverless-http');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
+const port = process.env.PORT || 8080;
 
 mongoose.connect(
   'mongodb+srv://root:TFvJiWgCq3K7TvRa@cluster0.jghn2.mongodb.net/Cluster0?retryWrites=true&w=majority',
@@ -14,13 +14,16 @@ app.use(express.json());
 app.use(cors());
 
 const budgetsRouter = require('./routes/budgets');
-app.use(`/.netlify/functions/api/budgets`, budgetsRouter);
+app.use(`/budgets`, budgetsRouter);
 
 const transactionsRouter = require('./routes/transactions');
-app.use(`/.netlify/functions/api/transactions`, transactionsRouter);
+app.use(`/transactions`, transactionsRouter);
 
 const usersRouter = require('./routes/users');
-app.use(`/.netlify/functions/api/users`, usersRouter);
+app.use(`/users`, usersRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
 
 module.exports = app;
-module.exports.handler = serverless(app);
