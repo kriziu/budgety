9d03f43b8f1924c43e56eac3fe0d8f70c7c65729
kriziu <brunodzi07@gmail.com>
2021-08-25 15:59:46 +0200

    Added sorting transactions

--- src/pages/Payments/components/TransactionList/index.tsx
+++ src/pages/Payments/components/TransactionList/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 import { useSelector } from 'react-redux';
 
@@ -8,32 +8,51 @@ import Transaction from '../Transaction';
 import { List, Select } from './Elements';
 import '../../../../constant/style/animations.css';
 import { Label } from '../../../../components/Form/Elements';
-import { useState } from 'react';
 
 const TransactionList: FC = (): JSX.Element => {
   const transactions = useSelector((state: RootState) => state.transactions);
   const budgets = useSelector((state: RootState) => state.budgets);
   const [selectedBudgetId, setSelectedBudgetId] = useState('');
+  const [selectedSort, setSelectedSort] = useState('date');
 
   const renderTransactions = (): (JSX.Element | undefined)[] => {
-    return transactions.map(transaction => {
-      if (!selectedBudgetId)
-        return (
-          <CSSTransition timeout={200} classNames="slide" key={transaction._id}>
-            <li>
-              <Transaction {...transaction} />
-            </li>
-          </CSSTransition>
-        );
-      else if (transaction.budgetId === selectedBudgetId)
-        return (
-          <CSSTransition timeout={200} classNames="slide" key={transaction._id}>
-            <li>
-              <Transaction {...transaction} />
-            </li>
-          </CSSTransition>
-        );
-      return undefined;
+    const selectedTransactions = selectedBudgetId
+      ? transactions.filter(
+          transaction => transaction.budgetId === selectedBudgetId
+        )
+      : transactions;
+
+    selectedTransactions.sort((a, b) => {
+      let compare: number;
+
+      switch (selectedSort.split('_')[0]) {
+        case 'title':
+          compare = a.title.localeCompare(b.title);
+          break;
+        case 'date':
+          compare = a.date > b.date ? -1 : a.date < b.date ? 1 : 0;
+          break;
+        case 'amount':
+          compare = a.amount > b.amount ? -1 : a.amount < b.amount ? 1 : 0;
+          break;
+        default:
+          compare = 0;
+          break;
+      }
+
+      return compare;
+    });
+
+    if (selectedSort.split('_')[1] === 'r') selectedTransactions.reverse();
+
+    return selectedTransactions.map(transaction => {
+      return (
+        <CSSTransition timeout={200} classNames="slide" key={transaction._id}>
+          <li>
+            <Transaction {...transaction} />
+          </li>
+        </CSSTransition>
+      );
     });
   };
 
@@ -46,18 +65,33 @@ const TransactionList: FC = (): JSX.Element => {
     ));
   };
 
-  const handleSelectChange = (
+  const handleSelectFilterChange = (
     e: React.ChangeEvent<HTMLSelectElement>
   ): void => {
     setSelectedBudgetId(e.target.value);
   };
 
+  const handleSelectSortChange = (
+    e: React.ChangeEvent<HTMLSelectElement>
+  ): void => {
+    setSelectedSort(e.target.value);
+  };
+
   return (
     <div style={{ margin: '4rem auto', padding: '0 4rem' }}>
       <Label>Filter transactions</Label>
-      <Select onChange={handleSelectChange}>
+      <Select onChange={handleSelectFilterChange} value={selectedBudgetId}>
         <option value="">All</option>
         {renderOptions()}
+      </Select>
+      <Label style={{ marginTop: '3rem' }}>Sort by</Label>
+      <Select onChange={handleSelectSortChange} value={selectedSort}>
+        <option value="date">Newest</option>
+        <option value="date_r">Oldest</option>
+        <option value="title">A...Z</option>
+        <option value="title_r">Z...A</option>
+        <option value="amount">Most money</option>
+        <option value="amount_r">Least money</option>
       </Select>
       <List>
         <TransitionGroup component={null}>
